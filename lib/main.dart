a2e0f861021e0256d9c2d54460898ee6f641fab7
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2021-11-26 23:31:10 +0100

    added functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'recipe.dart';
 
 void main() {
   runApp(const RecipeApp());
---
+++ lib/recipe.dart
@@ -0,0 +1,8 @@
+class Recipe {
+  String label;
+  String imgeUrl;
+
+  Recipe(this.label, this.imgeUrl);
+
+  static List<Recipe> samples = [Recipe('Spaghetti and Meatballs', 'assets/')];
+}
