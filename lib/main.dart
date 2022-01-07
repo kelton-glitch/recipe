2e205d95817ceeeecaf037f6639468d2294725c6
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2022-01-08 00:46:06 +0100

    Adding Recipe details

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'recipe.dart';
+import 'recipe_detail.dart';
 
 void main() {
   runApp(const RecipeApp());
@@ -42,7 +43,19 @@ class _MyHomePageState extends State<MyHomePage> {
         child: ListView.builder(
           itemCount: Recipe.samples.length,
           itemBuilder: (BuildContext context, int index) {
-            return buildRecipeCard(Recipe.samples[index]);
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return RecipeDetail( recipe: Recipe.samples[index]);
+                    },
+                  ),
+                );
+              },
+              child: buildRecipeCard(Recipe.samples[index]),
+            );
           },
         ),
       ),
--- lib/recipe.dart
+++ lib/recipe.dart
@@ -1,15 +1,55 @@
 class Recipe {
+  int servings;
+  List<Ingredient> ingredients;
   String label;
   String imageUrl;
 
-  Recipe(this.label, this.imageUrl);
+  Recipe(this.label, this.imageUrl, this.servings, this.ingredients);
 
   static List<Recipe> samples = [
-    Recipe('Achu', 'images/achu.jpg'),
-    Recipe('Eru', 'images/eru.jpg'),
-    Recipe('Roasted Fish', 'images/fish.jpg'),
-    Recipe('Jellof Rice', 'images/jellof.jpg'),
-    Recipe('Kwacoco Bible', 'images/kwacoco.jpg'),
-    Recipe('Sanga', 'images/sanga.jpg'),
-    ];
+    Recipe('Achu', 'images/achu.jpg',3,
+    [
+      Ingredient(2, 'kgs', 'meat'),
+      Ingredient(2, 'kgs', 'towel'),
+      Ingredient(2, 'kgs', 'canda'),
+    ]),
+    Recipe('Eru', 'images/eru.jpg',3,
+    [
+      Ingredient(2, 'kgs', 'meat'),
+      Ingredient(2, 'kgs', 'towel'),
+      Ingredient(2, 'kgs', 'canda'),
+    ]),
+    Recipe('Roasted Fish', 'images/fish.jpg',3,
+    [
+      Ingredient(2, 'kgs', 'meat'),
+      Ingredient(2, 'kgs', 'towel'),
+      Ingredient(2, 'kgs', 'canda'),
+    ]),
+    Recipe('Jellof Rice', 'images/jellof.jpg',3,
+    [
+      Ingredient(2, 'kgs', 'meat'),
+      Ingredient(2, 'kgs', 'towel'),
+      Ingredient(2, 'kgs', 'canda'),
+    ]),
+    Recipe('Kwacoco Bible', 'images/kwacoco.jpg',3,
+    [
+      Ingredient(2, 'kgs', 'meat'),
+      Ingredient(2, 'kgs', 'towel'),
+      Ingredient(2, 'kgs', 'canda'),
+    ]),
+    Recipe('Sanga', 'images/sanga.jpg',3,
+    [
+      Ingredient(2, 'kgs', 'meat'),
+      Ingredient(2, 'kgs', 'towel'),
+      Ingredient(2, 'kgs', 'canda'),
+    ]),
+  ];
+}
+
+class Ingredient {
+  double quantity;
+  String measure;
+  String name;
+
+  Ingredient(this.quantity, this.measure, this.name);
 }
