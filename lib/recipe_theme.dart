fc0c1006749ac0e30f1b4986edf8852746bb3280
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2022-01-11 20:44:13 +0100

    commenting out the theme

--- lib/recipe_theme.dart
+++ lib/recipe_theme.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/* import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class RecipeTheme {
@@ -49,3 +49,4 @@ class RecipeTheme {
     );
   }
 }
+ */
